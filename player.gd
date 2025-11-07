c0e6fb9cf2e1f8b1c75ec127aaa09acf228dd2a4
strawberry-p <strawberry@example.com>
2025-11-07 21:42:39 +0100

    added double jumps (yes) (this is actually really easy?)

--- player.gd
+++ player.gd
@@ -3,16 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 280.0
 const JUMP_VELOCITY = -400.0
+var double_jump = 2
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		double_jump = 2
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or double_jump > 0):
 		velocity.y = JUMP_VELOCITY
+		double_jump -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
