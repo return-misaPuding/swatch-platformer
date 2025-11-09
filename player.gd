9ec17db2a25477b6134628e762e8012b50f04743
strawberry-p <strawberry@example.com>
2025-11-09 11:10:25 +0100

    added color-based collision

--- player.gd
+++ player.gd
@@ -5,9 +5,17 @@ const SPEED = 280.0
 const JUMP_VELOCITY = -400.0
 var double_jump = 2
 const COLOR_STOP = 4
-var color_counter = 0
+var color_counter = 1
+var first_frame = true
+func inv_col_mask(no_collide: int) -> int:
+	var full = 0b1111
+	var res = full-2**(no_collide-1)
+	collision_mask = res
+	return res
 
 func _physics_process(delta: float) -> void:
+	if first_frame:
+		pass
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,7 +26,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or double_jump > 0):
 		velocity.y = JUMP_VELOCITY
 		double_jump -= 1
-
+	if Input.is_action_just_pressed("color_forward"):
+		if color_counter >= COLOR_STOP:
+			color_counter = 1 #overflow behavior
+		else:
+			color_counter += 1
+		if color_counter == 1:
+			collision_mask = 0b1111 #make all color tiles collide
+		else:
+			inv_col_mask(color_counter)
+		print(color_counter)
+		print(collision_mask)
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
