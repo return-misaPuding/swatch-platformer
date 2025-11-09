3435501de98a220148f5f2cd6457ed36c7376c8f
strawberry-p <strawberry@example.com>
2025-11-09 12:01:59 +0100

    Added color changing with effects on the player sprite, changed tile images

--- player.gd
+++ player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
-
-const SPEED = 280.0
+@export var red_sprite: Texture2D
+@export var yellow_sprite: Texture2D
+@export var blue_sprite: Texture2D
+@export var colorless_sprite: Texture2D
+@onready var sprite_array = [colorless_sprite,yellow_sprite, red_sprite, blue_sprite]
+@onready var sprite = $Sprite2D
+const MAIN_SPEED = 280.0
+var SPEED = MAIN_SPEED
 const JUMP_VELOCITY = -400.0
 var double_jump = 2
+var velocity_cancel_charge = 3
 const COLOR_STOP = 4
 var color_counter = 1
 var first_frame = true
@@ -35,9 +42,13 @@ func _physics_process(delta: float) -> void:
 			collision_mask = 0b1111 #make all color tiles collide
 		else:
 			inv_col_mask(color_counter)
+		sprite.texture = sprite_array[color_counter-1]
 		print(color_counter)
 		print(collision_mask)
-			
+		
+	
+	if Input.is_action_just_pressed("velocity_cancel"):
+		pass
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
